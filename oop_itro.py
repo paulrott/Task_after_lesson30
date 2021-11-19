5fa458daa7069784c5a7cf4a89e194b2821d7e78
paulrott <93085776+paulrott@users.noreply.github.com>
2021-11-19 23:57:54 +0300

    Update oop_itro.py

--- oop_itro.py
+++ oop_itro.py
@@ -1,23 +1,77 @@
-class Car:
+# ingredients = {'eggs': '2 pcs', 'milk': '60 g', 'salt':'20g'}
+# print(ingredients['eggs'])
 
-    wheels_number = 4
+class Recipe:
 
-    def __init__(self,name,color,year,is_crashed):
-        self.name = name
-        self.color = color
-        self.year = year
-        self.is_crashed = is_crashed
+   number_of_cooking_stages = 3
+
+   def __init__(self,name,ingredients,annotation,cooking_time,cooking_device ):
+      self.name = name
+      self.ingredients = ingredients
+      self.annotation = annotation
+      self.cooking_time = cooking_time
+      self.cooking_device = cooking_device
 
 
-mazda_car = Car(name='Mazda CX7', color='red',year=2017, is_crashed=True)
+recipe_1 = Recipe(name='Chicken soup', ingredients='potato,chiken breast,salt,carrot,onion', annotation = 'vegetable soup with chicken breast',cooking_time = '40',cooking_device =
+'RMC-M903S')
 
-print(mazda_car.name)
-print(mazda_car.is_crashed)
-print(mazda_car.wheels_number)
+print(recipe_1.name)
+print(recipe_1.ingredients)
+print(recipe_1.annotation)
+print(recipe_1.cooking_time)
+print(recipe_1.cooking_device)
 
-bmw_car = Car(name='BMW',color='black',year=2018,is_crashed=False)
 
-print(bmw_car.name,bmw_car.color,bmw_car.year,bmw_car.wheels_number)
 
-number_of_wheels_of_3_cars = Car.wheels_number * 3
-print(number_of_wheels_of_3_cars)
+recipe_2 = Recipe(name='hot dog', ingredients='sausage,bun,ketchup,mayonnaise', annotation = 'wheat bun with sausage seasoned with ketchup, mayonnaise, mustard',cooking_time = '10',cooking_device =
+'none')
+
+print(recipe_2.name)
+print(recipe_2.ingredients)
+print(recipe_2.annotation)
+print(recipe_2.cooking_time)
+print(recipe_2.cooking_device)
+
+
+
+recipe_3 = Recipe(name='омлет', ingredients='milk,eggs,milk', annotation = 'нежный омлет с молоком',cooking_time = '20',cooking_device = 'SkyBaker')
+
+print(recipe_3.name)
+print(recipe_3.ingredients)
+print(recipe_3.annotation)
+print(recipe_3.cooking_time)
+print(recipe_3.cooking_device)
+
+recipe_1 = {
+   'name': 'chicken soup',
+   'annotation': 'vegetable soup with chicken breast',
+   'cooking time':40,
+   'number of cooking stages': 3,
+   'ingredients':['potato', 'chiken breast', 'salt','carrot','onion'],
+   'stages of preparation': {'broth':'pour a saucepan with clean water, put chicken breast in it', 'vegetables':'cut into small pieces'}
+
+}
+print(("\033[35m {}" .format(recipe_1)))
+
+recipe_2 = {
+   'name': 'hot dog',
+   'annotation': 'wheat bun with sausage seasoned with ketchup, mayonnaise, mustard',
+   'cooking time':10,
+   'number of cooking stages': 3,
+   'ingredients':['sausage', 'bun', 'ketchup','mayonnaise'],
+   'stages of preparation': {'bun':'Ножом сделать надрез вдоль булочек до половины глубины','sausage': 'Сосиски отварить или поджарить', 'компоновка':'положить сосиски на булочку и залить кетчупов и майонезом по вкусу'}
+
+}
+print(("\033[34m {}" .format(recipe_2)))
+
+recipe_3 = {
+   'name': 'омлет',
+   'annotation': 'нежный омлет с молоком',
+   'cooking time':20,
+   'number of cooking stages': 3,
+   'ingredients':['milk','eggs','milk'],
+   'stages of preparation': {'eggs':'разбить яйца в миску', 'milk': 'залить яйца молоком', 'запекание':'взболтать и выложить в форму'}
+
+}
+print(("\033[31m {}" .format(recipe_3)))
